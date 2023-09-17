3a45b1966b56d0e0dd826ef6bb414c3db49b75f5
Tigist Wondimneh <wondimnehtigist@gmail.com>
2023-09-17 20:32:42 +0300

    adding improved chat feature

--- app.py
+++ app.py
@@ -1,15 +1,45 @@
-from flask import Flask, Blueprint
-from chat import chat
+from flask import Flask, Blueprint,jsonify, request
+from chat import chat_lagchain, load_new_conv
+import logging
+import json
 app = Flask(__name__)
 main_bp2 = Blueprint('main', __name__)
+main_bp = Blueprint('main', __name__)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 @main_bp2.route('/', methods=['GET'])
 def index():
     return "200"
 
 
-@main_bp2.route('/api', methods=['GET'])
+@main_bp2.route('/api', methods=['POST'])
 def api_route():
-    return chat()
+    data = request.json
+    response = {
+        "data" : None,
+        "error" : None
+    }
+    statusCode = 404
+    
+    try:
+        address = request.headers['address']
+        message = data['message']
+        conversation = load_new_conv(address)
+        output = conversation.predict(input=message)
+        logging.info(output)
+        response['data'] = json.loads(output)
+        statusCode = 200
+        
+    except Exception as error:
+        logging.error(error)
+        response['error'] = {
+        'message': f"{error}"
+        }
+        # response = error
+        statusCode = 404
+        
+    print(response)
+    return jsonify(response), statusCode
 
 app.register_blueprint(main_bp2)
 if __name__ == '__main__':
--- chat.py
+++ chat.py
@@ -1,18 +1,68 @@
 import openai
-from dotenv import load_dotenv
 import os
-def chat():
-    load_dotenv()
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    MessagesPlaceholder,
+    HumanMessagePromptTemplate,
+)
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationChain
+from cachetools import TTLCache
+CACHE_SIZE, CACHE_TIME = int(os.getenv("CACHE_SIZE")), int(os.getenv("CACHE_TIME"))
+
+current_dir = os.path.dirname(__file__)
+cache = TTLCache(maxsize=CACHE_SIZE, ttl=CACHE_TIME)
+
+def load_templates(filename):
+    f = open(filename)
+    return f.read()
+def load_new_conv(address):
+    if address in cache:
+        return cache[address]
+    cache[address] = chat_lagchain()
+    return cache[address]
+
+def chat_lagchain():
+    openai.api_key=os.getenv("OPENAI_KEY")
+    chat = ChatOpenAI(temperature=0)
+
+    sys_instructions_file = os.path.join(os.path.dirname(__file__),'ff.txt')
+    hum_instructions_file = os.path.join(os.path.dirname(__file__),'human.txt')
+    system_template = load_templates(sys_instructions_file)
+    human_template = load_templates(hum_instructions_file)
     
-    openai.api_key = os.getenv("OPENAI_KEY")
-    conversation = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "What's the weather like in africa?"},
-    ]
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",  # Use the appropriate model name
-        messages=conversation
-    )
-    assistant_reply = response['choices'][0]['message']['content']
-    # print(assistant_reply)
-    return assistant_reply
+
+    system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+    system_message_prompt.format_messages()
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    
+    chat_prompt = ChatPromptTemplate.from_messages([
+        system_message_prompt,
+        MessagesPlaceholder(variable_name="history"),
+        human_message_prompt
+    ])
+
+    memory = ConversationBufferMemory(input_key = "input", output_key="response", return_messages=True)
+    conversation = ConversationChain(memory=memory, prompt=chat_prompt, llm=chat)
+    return conversation
+
+ 
+            
+         
+# def chat():
+#     load_dotenv()
+    
+#     openai.api_key = os.getenv("OPENAI_KEY")
+#     conversation = [
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {"role": "user", "content": "What's the weather like in africa?"},
+#     ]
+#     response = openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",  # Use the appropriate model name
+#         messages=conversation
+#     )
+#     assistant_reply = response['choices'][0]['message']['content']
+#     # print(assistant_reply)
+#     return assistant_reply
